f6e685da130bd4e8dc452cb735434bf431c26fd3
Gomez Emmanuel <emmanuel.gomez.3@gmail.com>
2022-03-25 22:33:55 -0300

    Configuracion Entidades Country Category

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
